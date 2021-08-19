cedff81f42be9c6f2285cdeab1141472f9a26ed0
guoyaochen96 <guoyaochen96@163.com>
2021-08-19 17:31:05 +0800

    Generate  Json

--- main.py
+++ main.py
@@ -4,10 +4,40 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-import networkx as nx
-import numpy as np
-import matplotlib.pyplot as plt
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import QtGui
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.resize(350, 250)
+        self.center()
+
+        self.setWindowIcon(QtGui.QIcon("UnionPay.png"))
+
+        self.setWindowTitle('慧图示例')
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = self.screen().availableGeometry().center()
+
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    pass
+    main()
